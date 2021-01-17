c2adae46bfc2bfac2d86a13b72adb7303601b055
MarionJS <marion.playout@gmail.com>
2021-01-17 21:23:14 +0100

    APIRest second draft

--- src/main/java/com/example/APIRest/ClientController.java
+++ src/main/java/com/example/APIRest/ClientController.java
@@ -3,12 +3,16 @@ package com.example.APIRest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
-public class ClientControler {
+@RestController //on détermine le role de la classe
+public class ClientController {
 
     @Autowired
     private ClientRepository clients;
+    //quand l'application démarre je n'ai pas besoin de créer un nouveau client de ClientRepository
+    // il gère la fougère (initie le clientRepository, le clientController)
 
     @GetMapping(path = "/clients", produces = "application/json")
     public Iterable <Client> getClients(
--- src/main/java/com/example/APIRest/ClientRepository.java
+++ src/main/java/com/example/APIRest/ClientRepository.java
@@ -1,8 +1,10 @@
 package com.example.APIRest;
 import org.springframework.data.repository.CrudRepository;
-
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
+
+@Repository
 public interface ClientRepository extends CrudRepository <Client, String>{
 
     List<Client> findByAll( String guid, String firstName, String lastName, String street, String city, int zip );
